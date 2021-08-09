bb52f5beb7c7ccdaa82496ebf2816ebf15cb2989
Bilal Yaqub <bilal._.yaqub@hotmail.com>
2021-08-09 01:49:11 +0100

    Added some more functionality

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-let selection = document.querySelector('#gameSelection')
 let button = document.querySelector('button')
+let selection = document.querySelector('#gameSelection')
+let footballForm = document.querySelector('#footballForm')
+let tableTennisForm = document.querySelector('#tableTennisForm')
 
-let footballImage = 'https://lh3.googleusercontent.com/proxy/gKokMvyPADQXk45bBdC46k6seUvnCj5y2xAFqDzPsJaRnRmAaU8-8nicgWfd0hUBlz4VxitxGZ5cjK8TlHLN_ciJNU4teBoj_vnZC9CFR0zeVrdOrdQkmA'
 let tableTennisImage = 'http://pngimg.com/uploads/ping_pong/ping_pong_PNG102675.png'
+let footballImage = 'https://lh3.googleusercontent.com/proxy/gKokMvyPADQXk45bBdC46k6seUvnCj5y2xAFqDzPsJaRnRmAaU8-8nicgWfd0hUBlz4VxitxGZ5cjK8TlHLN_ciJNU4teBoj_vnZC9CFR0zeVrdOrdQkmA'
 
-
-button.addEventListener('click', function (evt) {
-    evt.preventDefault()
-
-    console.dir(selection)
+function createImage() {
     if (selection.value != 'Please Choose a Game') {
         let image = document.createElement('img')
         image.classList.add('image')
@@ -18,4 +16,18 @@ button.addEventListener('click', function (evt) {
 
         document.body.append(image)
     }
+}
+
+button.addEventListener('click', function (evt) {
+    evt.preventDefault()
+    console.dir(selection)
+
+    createImage()
+
+    if (selection.value == 'Football') footballForm.style.display = 'block'
+    else tableTennisForm.style.display = 'block'
+}, { once: true })
+
+button.addEventListener('click', function (evt) {
+    evt.preventDefault()
 })
