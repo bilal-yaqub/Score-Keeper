7ab656dd77757e661707faaca3c1343c5ecfb909
Bilal Yaqub <bilal._.yaqub@hotmail.com>
2021-08-09 02:52:09 +0100

    Added functionality for the checkboxes

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ let selection = document.querySelector('#gameSelection')
 let footballForm = document.querySelector('#footballForm')
 let tableTennisForm = document.querySelector('#tableTennisForm')
 
+let footballFormSelections = [footballForm.one, footballForm.three, footballForm.five]
+let tableTennisFormSelections = [tableTennisForm.five, tableTennisForm.ten, tableTennisForm.twenty]
+
 let tableTennisImage = 'http://pngimg.com/uploads/ping_pong/ping_pong_PNG102675.png'
 let footballImage = 'https://lh3.googleusercontent.com/proxy/gKokMvyPADQXk45bBdC46k6seUvnCj5y2xAFqDzPsJaRnRmAaU8-8nicgWfd0hUBlz4VxitxGZ5cjK8TlHLN_ciJNU4teBoj_vnZC9CFR0zeVrdOrdQkmA'
 
 function createImage() {
-    if (selection.value != 'Please Choose a Game') {
+    if (selection.gameSelectionChoice.value != 'Please Choose a Game') {
         let image = document.createElement('img')
         image.classList.add('image')
 
-        if (selection.value == 'Football') image.src = footballImage
+        if (selection.gameSelectionChoice.value == 'Football') image.src = footballImage
         else image.src = tableTennisImage
 
         document.body.append(image)
@@ -20,14 +23,24 @@ function createImage() {
 
 button.addEventListener('click', function (evt) {
     evt.preventDefault()
-    console.dir(selection)
 
     createImage()
 
-    if (selection.value == 'Football') footballForm.style.display = 'block'
+    if (selection.gameSelectionChoice.value == 'Football') footballForm.style.display = 'block'
     else tableTennisForm.style.display = 'block'
-}, { once: true })
 
-button.addEventListener('click', function (evt) {
-    evt.preventDefault()
+    selection.style.display = 'none'
 })
+
+for (let i = 0; i < 3; i++) {
+    let footballOption = footballFormSelections[i]
+    let tableTennisOption = tableTennisFormSelections[i]
+
+    footballOption.addEventListener('change', function () {
+        footballForm.style.display = 'none'
+    })
+
+    tableTennisOption.addEventListener('change', function () {
+        footballForm.style.display = 'none'
+    })
+}
