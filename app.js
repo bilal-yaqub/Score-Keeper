d697e536f50d8bac5ccaf88bc1069809220bb8dd
Bilal Yaqub <bilal._.yaqub@hotmail.com>
2021-08-11 17:32:24 +0100

    Finalised Basic Desgin

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let selection = document.querySelector('#gameSelection')
 let footballForm = document.querySelector('#footballForm')
 let scoreKeeper = document.querySelector('#scoreKeeper')
 let tableTennisForm = document.querySelector('#tableTennisForm')
+let playerOneName = document.querySelector('#playerOneName')
+let playerTwoName = document.querySelector('#playerTwoName')
 
 console.log(scoreKeeper.children)
 
@@ -21,33 +23,32 @@ let tableTennisImage = 'https://cdn.pixabay.com/photo/2014/04/02/17/00/ping-pong
 let footballImage = 'https://clipart-best.com/img/football/football-clip-art-12.png'
 
 function createImage() {
-    if (selection.gameSelectionChoice.value != 'Please Choose a Game') {
-        let image = document.createElement('img')
+    let image = document.createElement('img')
 
-
-        if (selection.gameSelectionChoice.value == 'Football') {
-            image.src = footballImage
-            image.id = 'footballImage'
-        } else {
-            image.src = tableTennisImage
-            image.id = 'tableTennisImage'
-        }
-
-        document.body.append(image)
+    if (selection.gameSelectionChoice.value == 'Football') {
+        image.src = footballImage
+        image.id = 'footballImage'
+    } else {
+        image.src = tableTennisImage
+        image.id = 'tableTennisImage'
     }
+
+    document.body.append(image)
 }
 
 button.addEventListener('click', function (evt) {
     evt.preventDefault()
 
-    createImage()
+    if (selection.gameSelectionChoice.value != 'Please Choose a Game') {
+        createImage()
 
-    if (selection.gameSelectionChoice.value == 'Football') {
-        footballSelected = true
-        footballForm.style.display = 'block'
-    } else tableTennisForm.style.display = 'block'
+        if (selection.gameSelectionChoice.value == 'Football') {
+            footballSelected = true
+            footballForm.style.display = 'block'
+        } else tableTennisForm.style.display = 'block'
 
-    selection.style.display = 'none'
+        selection.style.display = 'none'
+    }
 })
 
 for (let i = 0; i < 3; i++) {
@@ -91,21 +92,25 @@ for (let i = 0; i < 3; i++) {
 playerNames.addEventListener('submit', function (evt) {
     evt.preventDefault()
 
-    playerOne = playerNames.player1.value
-    playerTwo = playerNames.player2.value
+    if ((playerNames.player1.value != "") && (playerNames.player2.value != "")) {
+        playerOne = playerNames.player1.value
+        playerTwo = playerNames.player2.value
 
-    scoreKeeper.children[2].innerText = `${playerOne}  +1`
-    scoreKeeper.children[3].innerText = `${playerTwo}  +1`
+        scoreKeeper.children.playerOneBtn.innerText = `${playerOne}  +1`
+        scoreKeeper.children.playerTwoBtn.innerText = `${playerTwo}  +1`
+
+        playerOneName.innerText = playerOne
+        playerTwoName.innerText = playerTwo
 
 
-    playerNames.style.display = 'none'
-    scoreKeeper.style.display = 'block'
-
+        playerNames.style.display = 'none'
+        scoreKeeper.style.display = 'block'
+    }
 })
 
-scoreKeeper.children[2].addEventListener('click', function () {
+scoreKeeper.children.playerOneBtn.addEventListener('click', function () {
     playerOneScore += 1
-    console.log(firstTo)
+
     if (playerOneScore >= firstTo) {
         scoreKeeper.style.display = 'none'
         let text = document.createElement('p')
@@ -113,10 +118,10 @@ scoreKeeper.children[2].addEventListener('click', function () {
         document.body.append(text)
     }
 
-    scoreKeeper.children[0].innerText = `${playerOneScore} : ${playerTwoScore}`
+    scoreKeeper.children.showScore.innerText = `${playerOneScore} : ${playerTwoScore}`
 })
 
-scoreKeeper.children[3].addEventListener('click', function () {
+scoreKeeper.children.playerTwoBtn.addEventListener('click', function () {
     playerTwoScore += 1
     if (playerOneScore >= firstTo) {
         scoreKeeper.style.display = 'none'
@@ -125,10 +130,10 @@ scoreKeeper.children[3].addEventListener('click', function () {
         document.body.append(text)
     }
 
-    scoreKeeper.children[0].innerText = `${playerOneScore} : ${playerTwoScore}`
+    scoreKeeper.children.showScore.innerText = `${playerOneScore} : ${playerTwoScore}`
 })
-scoreKeeper.children[4].addEventListener('click', function () {
+scoreKeeper.children.resetBtn.addEventListener('click', function () {
     playerOneScore = 0
     playerTwoScore = 0
-    scoreKeeper.children[0].innerText = `${playerOneScore} : ${playerTwoScore}`
+    scoreKeeper.children.showScore.innerText = `${playerOneScore} : ${playerTwoScore}`
 })
